5c752de0decb6a09c791e9e30f1908ce54eb5935
Miguel Brown <miguel.a.brown@gmail.com>
2023-09-28 20:15:41 +0000

    :book: reorg to have before and after changes

--- phlat-1.0/dist/prepare.py
+++ phlat-1.0/dist/prepare.py
@@ -6,7 +6,7 @@
 # Compiled at: 2017-09-23 15:54:26
 import sys, subprocess, os, pysam
 
-def prepare(fastq1, fastq2, artindex, b2url, tag, outdir, phlatdir, ispe,nthreads=8):
+def prepare(fastq1, fastq2, artindex, b2url, tag, outdir, phlatdir, ispe, orientation, nthreads=8):
     tmpdir = outdir + '/' + tag + '.tmp'
     srcdir = phlatdir + '/dist/resources'
     if fastq1 == '' and fastq2 == '':
@@ -33,17 +33,17 @@ def prepare(fastq1, fastq2, artindex, b2url, tag, outdir, phlatdir, ispe,nthread
         print 'mkdir ' + tmpdir
         print '.....Process Bowtie 2 mapping on ' + tag + '.......\n'
         if ispe and fastq1 != '' and fastq2 != '':
-            cmd = b2url + ' ' + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -1 ' + fastq1 + ' -2 ' + fastq2 + ' |grep -P "\\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\\tchr" >' + tmpdir + '/' + tag + '.sam'
+            cmd = b2url + ' ' + str(orientation) + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -1 ' + fastq1 + ' -2 ' + fastq2 + ' |grep -P "\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\tchr" >' + tmpdir + '/' + tag + '.sam'
             subprocess.call(cmd, shell=True)
             print cmd
         if not ispe and fastq2 == '':
-            cmd = b2url + ' ' + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -U ' + fastq1 + '|grep -P "\\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\\tchr" > ' + tmpdir + '/' + tag + '.sam'
+            cmd = b2url + ' ' + str(orientation) + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -U ' + fastq1 + '|grep -P "\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\tchr" > ' + tmpdir + '/' + tag + '.sam'
             subprocess.call(cmd, shell=True)
             print cmd
         if not ispe and fastq1 != '' and fastq2 != '':
-            cmd = b2url + ' ' + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -U ' + fastq1 + '|grep -P "\\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\\tchr" |awk  -F"\\t" \'BEGIN {OFS="\\t"} {$1=$1."_1"}{print $0}\' - >' + tmpdir + '/' + tag + '.tmp1.sam'
+            cmd = b2url + ' ' + str(orientation) + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -U ' + fastq1 + '|grep -P "\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\tchr" |awk  -F"\t" \'BEGIN {OFS="\t"} {$1=$1."_1"}{print $0}\' - >' + tmpdir + '/' + tag + '.tmp1.sam'
             subprocess.call(cmd, shell=True)
-            cmd = b2url + ' ' + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -U ' + fastq2 + '|grep -P "\\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\\tchr" |awk  -F"\\t" \'BEGIN {OFS="\\t"} {$1=$1."_2"}{print $0}\' - > ' + tmpdir + '/' + tag + '.tmp2.sam'
+            cmd = b2url + ' ' + str(orientation) + ' --maxins 450 --no-mixed --no-discordant --no-unal -p ' + str(nthreads) + ' -D 20 -R 3 -N 0 -L 20 -i S,1,0.50 -x ' + artindex + '/ucsc.artHLA -U ' + fastq2 + '|grep -P "\t(A\\*|B\\*|C\\*|DQA1\\*|DQB1\\*|DRB1\\*)" |grep -P -v "\tchr" |awk  -F"\t" \'BEGIN {OFS="\t"} {$1=$1."_2"}{print $0}\' - > ' + tmpdir + '/' + tag + '.tmp2.sam'
             subprocess.call(cmd, shell=True)
             print cmd
             subprocess.call('cat ' + tmpdir + '/' + tag + '.tmp1.sam ' + tmpdir + '/' + tag + '.tmp2.sam >' + tmpdir + '/' + tag + '.sam ', shell=True)
@@ -51,21 +51,15 @@ def prepare(fastq1, fastq2, artindex, b2url, tag, outdir, phlatdir, ispe,nthread
         cmd = 'cut -f 3 ' + tmpdir + '/' + tag + '.sam |grep -P "(A|B|C|DQA1|DQB1|DRB1)" > ' + tmpdir + '/' + tag + '_HLA.map'
         subprocess.call(cmd, shell=True)
         print cmd
-        cmd = 'awk  -F"\\t" \'BEGIN {OFS="\\t"} {if($3~/^A/){$4=$4-1+29910331;$8=$8-1+29910331}} {if($3~/^B/){$4=$4-1+31322260;$8=$8-1+31322260}}{if($3~/^C/){$4=$4-1+31236946;$8=$8-1+31236946}} {if($3~/^DQA1/){$4=$4-1+32605236;$8=$8-1+32605236}} {if($3~/^DQB1/){$4=$4-1+32628013;$8=$8-1+32628013}} {if($3~/^DRB1/){$4=$4-1+32546868;$8=$8-1+32546868}} {if($3~/^(A|B|C|D)/){$3="chr6"}} {$5="255"}{print $0}\' ' + tmpdir + '/' + tag + '.sam' + '|cat ' + srcdir + '/bowtie.header - >' + tmpdir + '/' + tag + '.tmp.sam'
+        cmd = 'awk  -F"\t" \'BEGIN {OFS="\t"} {if($3~/^A/){$4=$4-1+29910331;$8=$8-1+29910331}} {if($3~/^B/){$4=$4-1+31322260;$8=$8-1+31322260}}{if($3~/^C/){$4=$4-1+31236946;$8=$8-1+31236946}} {if($3~/^DQA1/){$4=$4-1+32605236;$8=$8-1+32605236}} {if($3~/^DQB1/){$4=$4-1+32628013;$8=$8-1+32628013}} {if($3~/^DRB1/){$4=$4-1+32546868;$8=$8-1+32546868}} {if($3~/^(A|B|C|D)/){$3="chr6"}} {$5="255"}{print $0}\' ' + tmpdir + '/' + tag + '.sam' + '|cat ' + srcdir + '/bowtie.header - >' + tmpdir + '/' + tag + '.tmp.sam'
         subprocess.call(cmd, shell=True)
         print cmd
-        #pysam.view('-S', tmpdir + '/' + tag + '.tmp.sam', '-b', '-o' + tmpdir + '/' + tag + '.tmp.bam')
-        #pysam.sort('-n', tmpdir + '/' + tag + '.tmp.bam', tmpdir + '/' + tag + '_HLA.qnsorted')
-        cmd = 'samtools view -bS ' + tmpdir + '/' + tag + '.tmp.sam ' + ' > ' + tmpdir + '/' + tag + '.tmp.bam'
-        print cmd
-        subprocess.call(cmd, shell=True)
-        cmd = 'samtools sort -n -o ' + tmpdir + '/' + tag + '_HLA.qnsorted.bam ' + tmpdir + '/' + tag + '.tmp.sam'
-        print cmd
-        subprocess.call(cmd, shell=True)
+        pysam.view('-S', tmpdir + '/' + tag + '.tmp.sam', '-b', '-o' + tmpdir + '/' + tag + '.tmp.bam')
+        pysam.sort('-n', tmpdir + '/' + tag + '.tmp.bam', tmpdir + '/' + tag + '_HLA.qnsorted')
         #subprocess.call('rm ' + tmpdir + '/' + tag + '.tmp*', shell=True)
         #subprocess.call('rm ' + tmpdir + '/' + tag + '.sam*', shell=True)
     except:
         print 'execution failed\n'
-        #subprocess.call('rm -r ' + tmpdir, shell=True)
+        subprocess.call('rm -r ' + tmpdir, shell=True)
 
     return
